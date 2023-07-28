ca6c64d27e63b700fa39d6595c8ed01d9ab39648
Alexey Turulin <repakuku@icloud.com>
2023-07-28 14:57:55 -0400

    homework

--- MoveSlider/UISliderViewRepresentation.swift
+++ MoveSlider/UISliderViewRepresentation.swift
@@ -12,15 +12,38 @@ struct UISliderViewRepresentation: UIViewRepresentable {
     
     func makeUIView(context: Context) -> UISlider {
         let uiSlider = UISlider()
+        let alpha = Double(game.computeScore()) / 100.0
         
         uiSlider.minimumValue = 0
         uiSlider.maximumValue = 100
+        uiSlider.thumbTintColor = UIColor(
+            red: 255,
+            green: 0,
+            blue: 0,
+            alpha: alpha
+        )
+        uiSlider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.didValueChanged),
+            for: .valueChanged
+        )
         
         return uiSlider
     }
     
     func updateUIView(_ uiView: UISlider, context: Context) {
+        let alpha = Double(game.computeScore()) / 100.0
         uiView.setValue(Float(game.currentValue), animated: true)
+        uiView.thumbTintColor = UIColor(
+            red: 255,
+            green: 0,
+            blue: 0,
+            alpha: alpha
+        )
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(game: _game)
     }
     
 }
@@ -29,5 +52,19 @@ struct UISliderViewRepresentation_Previews: PreviewProvider {
     static var previews: some View {
         UISliderViewRepresentation()
             .environmentObject(Game())
+    }
+}
+
+extension UISliderViewRepresentation {
+    class Coordinator: NSObject {
+        @EnvironmentObject var game: Game
+        
+        init(game: EnvironmentObject<Game>) {
+            self._game = game
+        }
+        
+        @objc func didValueChanged(_ sender: UISlider) {
+            game.currentValue = Double(sender.value)
+        }
     }
 }
