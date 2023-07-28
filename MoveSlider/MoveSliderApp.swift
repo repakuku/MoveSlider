659e196f66502bd8cc6b8835464d01f846de111b
Alexey Turulin <repakuku@icloud.com>
2023-07-28 12:32:34 -0400

    Initial Commit

---
+++ MoveSlider/MoveSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveSliderApp.swift
+//  MoveSlider
+//
+//  Created by Алексей Турулин on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MoveSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
