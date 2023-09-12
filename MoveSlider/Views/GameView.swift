f6d9a4886de616718fa07f52ab5109ffbc5b441e
Alexey Turulin <repakuku@icloud.com>
2023-09-12 16:36:39 -0400

    changed background color

--- MoveSlider/Views/GameView.swift
+++ MoveSlider/Views/GameView.swift
@@ -11,30 +11,34 @@ struct GameView: View {
     @StateObject private var gameViewModel = GameViewModel()
     
     var body: some View {
-        VStack(spacing: 30) {
-            Text("Move slider as close to: \(gameViewModel.game.targetValue)")
-            
-            GameSliderView(gameViewModel: gameViewModel)
+        ZStack {
+            Color(uiColor: .systemFill)
+                .ignoresSafeArea()
+            VStack(spacing: 30) {
+                Text("Move slider as close to: \(gameViewModel.game.targetValue)")
+                
+                GameSliderView(gameViewModel: gameViewModel)
 
-            Button("Check me!") {
-                gameViewModel.showAlert()
-            }
-            .alert(
-                "Start Again",
-                isPresented: $gameViewModel.alertPresented
-            ) {
-                Button("Ok") {
+                Button("Check me!") {
+                    gameViewModel.showAlert()
+                }
+                .alert(
+                    "Start Again",
+                    isPresented: $gameViewModel.alertPresented
+                ) {
+                    Button("Ok") {
+                        gameViewModel.startNewGame()
+                    }
+                } message: {
+                    Text("Score: \(gameViewModel.scores)")
+                }
+                
+                Button("Start Again") {
                     gameViewModel.startNewGame()
                 }
-            } message: {
-                Text("Score: \(gameViewModel.scores)")
             }
-            
-            Button("Start Again") {
-                gameViewModel.startNewGame()
-            }
+            .padding()
         }
-        .padding()
     }
 }
 
